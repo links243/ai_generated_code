4928a3bace157fdd2232f40f64c95d8f1da4bc2a
links243 <33209197+links243@users.noreply.github.com>
2025-05-13 11:14:08 +0000

    修改

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -12,7 +11,6 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
